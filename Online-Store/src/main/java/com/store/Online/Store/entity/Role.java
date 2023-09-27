bd72b852e17ca40a4d2b05e3e8d6fb821e3d5a78
Sofaross <sofaross228@gmail.com>
2023-09-28 01:59:30 +0300

    Create entity fot Role

---
+++ Online-Store/src/main/java/com/store/Online/Store/entity/Role.java
@@ -0,0 +1,34 @@
+package com.store.Online.Store.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "role")
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @OneToMany(mappedBy = "roleId")
+    private Collection<User> user;
+
+    public Role() {
+    }
+
+}
