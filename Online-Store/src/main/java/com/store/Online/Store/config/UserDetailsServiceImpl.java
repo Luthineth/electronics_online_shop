4a4d1a2ffc3795ed010733b65c8dd56c05b92c19
Sofaross <sofaross228@gmail.com>
2023-10-04 16:31:16 +0300

    Start work with jwt

---
+++ Online-Store/src/main/java/com/store/Online/Store/config/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.store.Online.Store.config;
+
+import com.store.Online.Store.entity.User;
+import com.store.Online.Store.repository.userRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final userRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(userRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+//        return new org.springframework.security.core.userdetails.User(
+//                user.getEmail(), user.getPassword(), user.getAuthorities());
+    }
+
+}
