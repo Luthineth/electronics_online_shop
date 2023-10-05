43e51784939952cb0d97f3ab1ab0ae5875ed2726
Sofaross <sofaross228@gmail.com>
2023-10-05 15:18:15 +0300

    "Added SecurityConfig for JWT-based authentication and authorization."

--- Online-Store/src/main/java/com/store/Online/Store/config/SecurityConfig.java
+++ Online-Store/src/main/java/com/store/Online/Store/config/SecurityConfig.java
@@ -1,51 +1,53 @@
 package com.store.Online.Store.config;
 
+import com.store.Online.Store.config.jwt.JwtAuthenticationEntryPoint;
+import com.store.Online.Store.config.jwt.JwtSecurityConfigurer;
+import com.store.Online.Store.config.jwt.JwtTokenUtil;
+import com.store.Online.Store.config.jwt.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
 
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
+    private final JwtTokenUtil jwtTokenUtil;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
+
+    @Autowired
+    public SecurityConfig(JwtTokenUtil jwtTokenProvider) {
+        this.jwtTokenUtil = jwtTokenProvider;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated()
+        http.httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+                .authorizeRequests()
+                    .antMatchers("/authenticate").permitAll() // Allow access to authentication endpoint
+                    .antMatchers("/login").permitAll() // Allow access to login endpoint
+                    .anyRequest().authenticated() // All other requests require authentication
+                .and ()
+                .cors(Customizer.withDefaults())
+                .apply(new JwtSecurityConfigurer(jwtTokenUtil));
     }
 }
