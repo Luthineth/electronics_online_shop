efa77cc91d6633096a81172d93d4e125c21f2a3a
Sofaross <sofaross228@gmail.com>
2023-10-05 15:02:49 +0300

    The code represents the JwtAuthenticationEntryPoint class, which implements the AuthenticationEntryPoint interface in Spring Security. This component is an authentication error handler. In case of an authentication error, the commence() method sends an HTTP response to the client with a status code of 401 (Unauthorized) and the message "Unauthorized". Thus, this component is responsible for handling situations where a user tries to access a protected resource without proper authentication.

--- Online-Store/src/main/java/com/store/Online/Store/config/jwt/JwtAuthenticationEntryPoint.java
+++ Online-Store/src/main/java/com/store/Online/Store/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,8 @@
-package com.store.Online.Store.config;
+package com.store.Online.Store.config.jwt;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +12,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
