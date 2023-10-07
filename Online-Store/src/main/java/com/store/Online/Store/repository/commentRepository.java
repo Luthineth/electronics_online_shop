fa83f26165af6894f77c67f41e5b9e909bbb69ea
Sofaross <sofaross228@gmail.com>
2023-10-07 22:04:02 +0300

    add method delete imageUrl

--- Online-Store/src/main/java/com/store/Online/Store/repository/commentRepository.java
+++ Online-Store/src/main/java/com/store/Online/Store/repository/commentRepository.java
@@ -3,9 +3,19 @@ package com.store.Online.Store.repository;
 import com.store.Online.Store.entity.Comment;
 import com.store.Online.Store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface commentRepository extends JpaRepository<Comment, Long> {
+
     List<Comment> findByProductId(Product productId);
+
+    @Modifying
+    @Query("UPDATE Comment c SET c.imageUrl = null WHERE c.commentId = :commentId")
+    void deleteImageByCommentId(@Param("commentId") Long commentId);
 }
