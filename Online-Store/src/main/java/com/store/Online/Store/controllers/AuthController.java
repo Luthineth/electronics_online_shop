37ff597e781b44a430a0715a9ab4288ec28df79e
Sofaross <sofaross228@gmail.com>
2023-10-07 22:44:11 +0300

    create auth controller with authentication and login users

---
+++ Online-Store/src/main/java/com/store/Online/Store/controllers/AuthController.java
@@ -0,0 +1,53 @@
+package com.store.Online.Store.controllers;
+
+import com.store.Online.Store.dto.AuthenticationRequest;
+import com.store.Online.Store.dto.UserRequest;
+import com.store.Online.Store.exception.RoleDefinitionException;
+import com.store.Online.Store.exception.UserCreationException;
+import com.store.Online.Store.service.userService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestController
+public class AuthController {
+
+    private final userService userService;
+
+    public AuthController(com.store.Online.Store.service.userService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/authenticate",
+                 consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody UserRequest user) {
+        try {
+            userService.register(user);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (UserCreationException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (RoleDefinitionException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    @PostMapping(value = "/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest authenticationRequest) {
+        try{
+            Map<String, String> response = userService.login(authenticationRequest);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
