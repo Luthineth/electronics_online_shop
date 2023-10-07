42a288e25ceb668289467a28553f37837edd5d32
Sofaross <sofaross228@gmail.com>
2023-10-07 22:07:32 +0300

    add method findProductsByCategoryId

--- Online-Store/src/main/java/com/store/Online/Store/repository/productCategoryRepository.java
+++ Online-Store/src/main/java/com/store/Online/Store/repository/productCategoryRepository.java
@@ -1,7 +1,17 @@
 package com.store.Online.Store.repository;
 
+import com.store.Online.Store.entity.Product;
 import com.store.Online.Store.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface productCategoryRepository extends JpaRepository<ProductCategory,Long> {
+
+    @Query("SELECT p FROM Product p JOIN p.productCategoryCollection pc WHERE pc.categoryId = :categoryId")
+    List<Product> findProductsByCategoryId(@Param("categoryId") Long categoryId);
 }
