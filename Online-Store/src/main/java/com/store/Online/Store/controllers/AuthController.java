b79d2fba7a679cee4579565a43a3810e65aba4ed
Sofaross <sofaross228@gmail.com>
2023-10-10 01:21:00 +0300

    adding error treatments

--- Online-Store/src/main/java/com/store/Online/Store/controllers/AuthController.java
+++ Online-Store/src/main/java/com/store/Online/Store/controllers/AuthController.java
@@ -4,10 +4,12 @@ import com.store.Online.Store.dto.AuthenticationRequest;
 import com.store.Online.Store.dto.UserRequest;
 import com.store.Online.Store.exception.RoleDefinitionException;
 import com.store.Online.Store.exception.UserCreationException;
+import com.store.Online.Store.exception.UserNotFoundException;
 import com.store.Online.Store.service.userService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,11 +45,13 @@ public class AuthController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest authenticationRequest) {
-        try{
+        try {
             Map<String, String> response = userService.login(authenticationRequest);
             return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (UserNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
         }
     }
 }
