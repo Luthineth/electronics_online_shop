159e44a9b271023f195d52ad749f37c9600d5083
Sofaross <sofaross228@gmail.com>
2023-10-11 00:49:26 +0300

    Added OrderServiceImpl class with order creation logic

---
+++ Online-Store/src/main/java/com/store/Online/Store/service/impl/OrderServiceImpl.java
@@ -0,0 +1,112 @@
+package com.store.Online.Store.service.impl;
+
+import com.store.Online.Store.dto.OrderItemRequest;
+import com.store.Online.Store.entity.Order;
+import com.store.Online.Store.entity.OrderItem;
+import com.store.Online.Store.entity.Product;
+import com.store.Online.Store.entity.User;
+import com.store.Online.Store.exception.UserNotFoundException;
+import com.store.Online.Store.repository.orderItemRepository;
+import com.store.Online.Store.repository.orderRepository;
+import com.store.Online.Store.repository.productRepository;
+import com.store.Online.Store.repository.userRepository;
+import com.store.Online.Store.service.orderService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderServiceImpl implements orderService {
+
+    private final orderRepository orderRepository;
+
+    private final productRepository productRepository;
+
+    private final userRepository userRepository;
+
+    private final orderItemRepository orderItemRepository;
+    private final EmailService emailService;
+
+    public OrderServiceImpl(com.store.Online.Store.repository.orderRepository orderRepository, com.store.Online.Store.repository.productRepository productRepository, com.store.Online.Store.repository.userRepository userRepository, com.store.Online.Store.repository.orderItemRepository orderItemRepository, EmailService emailService) {
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void createOrder(List<OrderItemRequest> orderItems) {
+        Order order = new Order();
+        User user;
+        order.setOrderDate(new Date());
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = auth.getName();
+        Optional<User> optionalUser = userRepository.findByEmail(userEmail);
+        if (optionalUser.isPresent()) {
+            user = optionalUser.get();
+        } else {
+          throw new UserNotFoundException("Нет пользователя");
+        }
+        order.setUserId(user);
+
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        int itemNumber = 1;
+
+        StringBuilder descriptionBuilder = new StringBuilder();
+
+        for (OrderItemRequest orderItemRequest : orderItems) {
+            Product product = productRepository.findById(orderItemRequest.getProductId()).orElse(null);
+
+            if (product != null) {
+                OrderItem orderItem = new OrderItem();
+                orderItem.setProductId(product);
+                orderItem.setQuantity(orderItemRequest.getQuantity());
+                orderItem.setOrderId(order);
+
+                descriptionBuilder.append(itemNumber).append(". ").append(product.getProductName()).append("\n");
+                descriptionBuilder.append("   - Количество: ").append(orderItemRequest.getQuantity()).append("\n");
+                descriptionBuilder.append("   - Стоимость: ").append(product.getPriceWithDiscount()).append(" ₽").append("\n\n");
+                itemNumber++;
+
+                BigDecimal productTotalPrice = product.getPriceWithDiscount().multiply(BigDecimal.valueOf(orderItemRequest.getQuantity()));
+                totalPrice = totalPrice.add(productTotalPrice);
+                orderItemRepository.save(orderItem);
+
+
+            }
+        }
+        order.setTotalPrice(totalPrice);
+        orderRepository.save(order);
+
+        for (OrderItemRequest orderItemRequest : orderItems) {
+            Product product = productRepository.findById(orderItemRequest.getProductId()).orElse(null);
+
+            if (product != null) {
+                int newStockQuantity = product.getStockQuantity() - orderItemRequest.getQuantity();
+                product.setStockQuantity(newStockQuantity);
+                productRepository.updateStockQuantity(product.getProductId(), newStockQuantity);
+            }
+        }
+
+        descriptionBuilder.append("Итоговая стоимость заказа: ").append(totalPrice).append(" ₽").append("\n\n");
+
+        descriptionBuilder.append("Мы рады быть вашими партнерами и готовы помочь вам в любое время. Если у вас есть какие-то вопросы или требуется дополнительная информация, пожалуйста, не стесняйтесь связаться с нами.\n\n");
+        descriptionBuilder.append("С уважением,\nКоманда ").append("Наш онлайн магазинчик");
+
+        descriptionBuilder.insert(0, "Спасибо за ваш заказ! Ваш заказ (номер " + order.getOrderId() + ") был успешно обработан. Ниже приведен список товаров в вашем заказе с указанием количества и стоимости каждого товара:\n\n");
+        descriptionBuilder.insert(0, "Уважаемый(ая) " + user.getSecondName() + " " + user.getFirstName() + ",\n\n");
+
+        String body = descriptionBuilder.toString();
+
+        String subject = "Ваш заказ и его стоимость";
+
+        emailService.sendEmail(userEmail,subject,body);
+    }
+}
