9f9486a54657e47211dce6e96a379c7d20b3b1c5
Sofaross <sofaross228@gmail.com>
2023-10-11 23:02:38 +0300

    changed the request

--- Online-Store/src/main/java/com/store/Online/Store/repository/productCategoryRepository.java
+++ Online-Store/src/main/java/com/store/Online/Store/repository/productCategoryRepository.java
@@ -4,14 +4,13 @@ import com.store.Online.Store.entity.Product;
 import com.store.Online.Store.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface productCategoryRepository extends JpaRepository<ProductCategory,Long> {
+public interface productCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
-    @Query("SELECT p FROM Product p JOIN p.productCategoryCollection pc WHERE pc.categoryId = :categoryId")
-    List<Product> findProductsByCategoryId(@Param("categoryId") Long categoryId);
+    @Query("SELECT pc.productId FROM ProductCategory pc WHERE pc.categoryId.categoryId = :categoryId")
+    List<Product> findByCategoryId(Long categoryId);
 }
