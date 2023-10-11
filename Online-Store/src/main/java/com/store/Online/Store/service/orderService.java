feb4a37beafbfa65bc2b1f460493de2026f53a39
Sofaross <sofaross228@gmail.com>
2023-10-11 22:58:36 +0300

    Add orderService interface for order-related operations Introduced the orderService interface to handle order-related operations. The interface includes a method to create an order based on a list of order item requests. The method is annotated with @Transactional to ensure proper transaction management during order creation.

--- Online-Store/src/main/java/com/store/Online/Store/service/orderService.java
+++ Online-Store/src/main/java/com/store/Online/Store/service/orderService.java
@@ -1,4 +1,11 @@
 package com.store.Online.Store.service;
 
+import com.store.Online.Store.dto.OrderItemRequest;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
 public interface orderService {
+
+    @Transactional
+    void createOrder(List<OrderItemRequest> orderItemRequests);
 }
