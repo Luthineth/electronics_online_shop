1f4d183e6b75b2f435aa3f3fdc39fe7a1515c16e
Sofaross <sofaross228@gmail.com>
2023-10-11 22:57:23 +0300

    delete import

--- Online-Store/src/main/java/com/store/Online/Store/repository/orderItemRepository.java
+++ Online-Store/src/main/java/com/store/Online/Store/repository/orderItemRepository.java
@@ -4,6 +4,8 @@ import com.store.Online.Store.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface orderItemRepository extends JpaRepository<OrderItem,Long> {
 }
