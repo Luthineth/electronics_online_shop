5faae7ab2d8e0370abbdbbd84e2523bee82add24
Sofaross <sofaross228@gmail.com>
2023-10-11 22:50:58 +0300

    Adding error handling, add getCommentsByProductId sort comments

--- Online-Store/src/main/java/com/store/Online/Store/controllers/CommentController.java
+++ Online-Store/src/main/java/com/store/Online/Store/controllers/CommentController.java
@@ -2,12 +2,16 @@ package com.store.Online.Store.controllers;
 
 import com.store.Online.Store.dto.CommentRequest;
 import com.store.Online.Store.entity.Comment;
+import com.store.Online.Store.exception.*;
 import com.store.Online.Store.service.commentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -22,28 +26,68 @@ public class CommentController {
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<List<Comment>> getCommentsByProductId(@PathVariable Long productId) {
-        List<Comment> comments = commentService.getCommentsByProductIdSortedByRating(productId);
-        return ResponseEntity.ok(comments);
+    public ResponseEntity<?> getCommentsByProductId(@PathVariable Long productId) {
+        try {
+            List<Comment> comments = commentService.getCommentsByProductIdSortedByRating(productId);
+            return ResponseEntity.ok(comments);
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving comments.");
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Comment> addComment(@RequestBody CommentRequest comment) {
-        Comment addedComment = commentService.addComment(comment);
-        return ResponseEntity.ok(addedComment);
+    public ResponseEntity<?> addComment(@RequestBody CommentRequest comment) {
+        try {
+            Comment addedComment = commentService.addComment(comment);
+            return ResponseEntity.ok(addedComment);
+        } catch (UserCreationException | ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while adding a comment.");
+        }
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
     @PutMapping("/{commentId}")
     public ResponseEntity<?> deleteImage(@PathVariable Long commentId) {
-        commentService.deleteImage(commentId);
-        return ResponseEntity.ok().build();
+        try {
+            commentService.deleteImage(commentId);
+            return ResponseEntity.ok().build();
+        } catch (CommentImageDeletionException | CommentNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the comment image.");
+        }
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
     @DeleteMapping("/{commentId}")
     public ResponseEntity<?> deleteComment(@PathVariable Long commentId) {
-        commentService.deleteComment(commentId);
-        return ResponseEntity.ok().build();
+        try {
+            commentService.deleteComment(commentId);
+            return ResponseEntity.ok().build();
+        } catch (CommentDeletionException | CommentNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the comment.");
+        }
+    }
+
+    @GetMapping("/{productId}/sorted")
+    public ResponseEntity<?> getCommentsByProductId(
+            @PathVariable Long productId,
+            @RequestParam(defaultValue = "ASC") Sort.Direction direction) {
+        try {
+            List<Comment> comments = commentService.getCommentsByProductIdSortedByRating(productId, direction);
+            return ResponseEntity.ok(comments);
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving comments.");
+        }
     }
 }
