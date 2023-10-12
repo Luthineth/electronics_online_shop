dac6d0dd5654b42e5e9ca8a87c89fb97db10d21e
Sofaross <sofaross228@gmail.com>
2023-10-12 18:42:14 +0300

    \

--- Online-Store/src/main/java/com/store/Online/Store/service/productCategoryService.java
+++ Online-Store/src/main/java/com/store/Online/Store/service/productCategoryService.java
@@ -5,6 +5,7 @@ import com.store.Online.Store.entity.Product;
 import java.util.List;
 
 public interface productCategoryService {
+
     List<Product> getProductsByCategoryAndSubcategories(Long categoryId);
 
     List<Product> getProductsRecursively(Long categoryId, List<Product> products);
