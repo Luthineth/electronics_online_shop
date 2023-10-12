5f299867a247dfa92999f34de5f4afaff4f2a428
Sofaross <sofaross228@gmail.com>
2023-10-12 18:40:08 +0300

    delete methods getCommentsByProductId

--- Online-Store/src/main/java/com/store/Online/Store/controllers/CommentController.java
+++ Online-Store/src/main/java/com/store/Online/Store/controllers/CommentController.java
@@ -5,14 +5,13 @@ import com.store.Online.Store.entity.Comment;
 import com.store.Online.Store.exception.*;
 import com.store.Online.Store.service.commentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/comments")
@@ -23,18 +22,6 @@ public class CommentController {
     @Autowired
     public CommentController(commentService commentService) {
         this.commentService = commentService;
-    }
-
-    @GetMapping("/{productId}")
-    public ResponseEntity<?> getCommentsByProductId(@PathVariable Long productId) {
-        try {
-            List<Comment> comments = commentService.getCommentsByProductIdSortedByRating(productId);
-            return ResponseEntity.ok(comments);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving comments.");
-        }
     }
 
     @PostMapping
@@ -74,20 +61,6 @@ public class CommentController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while deleting the comment.");
-        }
-    }
-
-    @GetMapping("/{productId}/sorted")
-    public ResponseEntity<?> getCommentsByProductId(
-            @PathVariable Long productId,
-            @RequestParam(defaultValue = "ASC") Sort.Direction direction) {
-        try {
-            List<Comment> comments = commentService.getCommentsByProductIdSortedByRating(productId, direction);
-            return ResponseEntity.ok(comments);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving comments.");
         }
     }
 }
