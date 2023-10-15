8f178e3ed709c44c7608c2682bcb427e3e1d9004
Sofaross <sofaross228@gmail.com>
2023-10-16 02:25:59 +0300

    adding error treatments for

---
+++ Online-Store/src/main/java/com/store/Online/Store/exception/CategoryAdditionException.java
@@ -0,0 +1,7 @@
+package com.store.Online.Store.exception;
+
+public class CategoryAdditionException extends RuntimeException {
+    public CategoryAdditionException(String message) {
+        super(message);
+    }
+}
---
+++ Online-Store/src/main/java/com/store/Online/Store/exception/CommentAdditionException.java
@@ -0,0 +1,7 @@
+package com.store.Online.Store.exception;
+
+public class CommentAdditionException extends RuntimeException {
+    public CommentAdditionException(String message) {
+        super(message);
+    }
+}
