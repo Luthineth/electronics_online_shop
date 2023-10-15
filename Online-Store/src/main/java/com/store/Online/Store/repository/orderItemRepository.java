3d7d364a76530e03f2dfa5ff470c71c4e6fee18e
Sofaross <sofaross228@gmail.com>
2023-10-16 01:56:14 +0300

    adding the deleteByProductId method

--- Online-Store/src/main/java/com/store/Online/Store/repository/orderItemRepository.java
+++ Online-Store/src/main/java/com/store/Online/Store/repository/orderItemRepository.java
@@ -1,6 +1,7 @@
 package com.store.Online.Store.repository;
 
 import com.store.Online.Store.entity.OrderItem;
+import com.store.Online.Store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.Collection;
 
 @Repository
 public interface orderItemRepository extends JpaRepository<OrderItem,Long> {
+    void deleteByProductId(Product product);
 }
