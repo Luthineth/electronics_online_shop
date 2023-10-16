2788e7057c0be4402af0491c47e06b26beb88c9d
Sofaross <sofaross228@gmail.com>
2023-10-16 17:29:08 +0300

    adding error handling

---
+++ Online-Store/src/main/java/com/store/Online/Store/exception/ProductUpdateException.java
@@ -0,0 +1,7 @@
+package com.store.Online.Store.exception;
+
+public class ProductUpdateException extends RuntimeException {
+    public ProductUpdateException(String message) {
+        super(message);
+    }
+}
