c1ebafc70adf433e239a87d2982fca6285e43a85
Sofaross <sofaross228@gmail.com>
2023-11-03 02:29:21 +0300

    add images

--- Online-Store/src/main/java/com/store/Online/Store/config/CorsConfig.java
+++ Online-Store/src/main/java/com/store/Online/Store/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.store.Online.Store.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -19,6 +20,17 @@ public class CorsConfig {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .maxAge(3600);
+            }
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer resourceConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/images/**")
+                        .addResourceLocations("D:\\javaCode\\electronics_online_shop\\images");
             }
         };
     }
