1fbc82d47d683c58c28eee17c1f4f6952a489d08
Sofaross <sofaross228@gmail.com>
2023-11-15 15:22:30 +0300

    removing an unnecessary method

--- Online-Store/src/main/java/com/store/Online/Store/service/impl/CategoryServiceImpl.java
+++ Online-Store/src/main/java/com/store/Online/Store/service/impl/CategoryServiceImpl.java
@@ -18,7 +18,7 @@ public class CategoryServiceImpl implements categoryService {
     private final productCategoryRepository productCategoryRepository;
 
     @Autowired
-    public CategoryServiceImpl(categoryRepository categoryRepository, com.store.Online.Store.repository.productCategoryRepository productCategoryRepository){
+    public CategoryServiceImpl(categoryRepository categoryRepository, com.store.Online.Store.repository.productCategoryRepository productCategoryRepository) {
         this.categoryRepository = categoryRepository;
         this.productCategoryRepository = productCategoryRepository;
     }
@@ -36,7 +36,23 @@ public class CategoryServiceImpl implements categoryService {
     @Override
     public Category addCategory(CategoryRequest categoryRequest) {
 
-        Category category = mapToCategory(categoryRequest);
+        Category category = new Category();
+
+        if (categoryRequest.getCategoryName() != null) {
+            category.setCategoryName(categoryRequest.getCategoryName());
+        } else {
+            throw new NoCategoryName("Enter the category name ");
+        }
+
+        if (categoryRequest.getParentCategoryId() == null) {
+            category.setParentCategoryId(null);
+        } else {
+            Category parentCategory = categoryRepository.findById(categoryRequest.getParentCategoryId())
+                    .orElseThrow(() -> new CategoryNotFoundException("Parent category not found with ID: " + categoryRequest.getParentCategoryId()));
+
+            parentCategory.setCategoryName(categoryRepository.findCategoryNameByCategoryId(categoryRequest.getParentCategoryId()));
+            category.setParentCategoryId(parentCategory);
+        }
 
         try {
             return categoryRepository.save(category);
@@ -85,7 +101,7 @@ public class CategoryServiceImpl implements categoryService {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new CategoryNotFoundException("Category not found with ID: " + categoryId));
         List<Category> subCategories = categoryRepository.findSubCategories(categoryId);
-        if (!subCategories.isEmpty() )
+        if (!subCategories.isEmpty())
             throw new SubCategoryNotFoundException("This category has subcategories, so you can't delete this category, first delete the subcategories");
         productCategoryRepository.deleteByCategoryId(category);
 
@@ -94,26 +110,5 @@ public class CategoryServiceImpl implements categoryService {
         } catch (Exception e) {
             throw new CategoryDeletionException("Failed to delete category. Reason: " + e.getMessage());
         }
-    }
-
-    public Category mapToCategory(CategoryRequest request) {
-        Category category = new Category();
-
-        if (request.getCategoryName() != null) {
-            category.setCategoryName(request.getCategoryName());
-        } else {
-            throw new NoCategoryName("Enter the category name ");
-        }
-
-        if (request.getParentCategoryId() == null) {
-            category.setParentCategoryId(null);
-        } else {
-            Category parentCategory = categoryRepository.findById(request.getParentCategoryId())
-                    .orElseThrow(() -> new CategoryNotFoundException("Parent category not found with ID: " + request.getParentCategoryId()));
-
-            parentCategory.setCategoryName(categoryRepository.findCategoryNameByCategoryId(request.getParentCategoryId()));
-            category.setParentCategoryId(parentCategory);
-        }
-        return category;
     }
 }
