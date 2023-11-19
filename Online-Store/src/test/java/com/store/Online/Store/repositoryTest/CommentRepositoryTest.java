27f1ac9d5c14cc8b2b14ad527f2e1d1ff86fa6eb
Sofaross <sofaross228@gmail.com>
2023-11-19 22:41:59 +0300

    Add comment repository test

--- Online-Store/src/test/java/com/store/Online/Store/repositoryTest/CommentRepositoryTest.java
+++ Online-Store/src/test/java/com/store/Online/Store/repositoryTest/CommentRepositoryTest.java
@@ -2,23 +2,17 @@ package com.store.Online.Store.repositoryTest;
 
 import com.store.Online.Store.entity.*;
 import com.store.Online.Store.repository.commentRepository;
-import com.store.Online.Store.repository.productRepository;
-import com.store.Online.Store.repository.userRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
